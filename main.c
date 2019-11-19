e713174162144372da220dae4e73d366a4b11e94
SAmohsen <48262280+SAmohsen@users.noreply.github.com>
2019-11-19 02:29:09 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+Pwm_ConfigType con = {
+
+};
+int main(int argc, char **argv) {
+	while(1){
+
+	}
+}
---
+++ main.h
@@ -0,0 +1,36 @@
+/*
+ * main.h
+ *
+ *  Created on: Nov 11, 2019
+ *      Author: sayed
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+#include "common_marcos.h"
+#include "micro_config.h"
+#include "std_types.h"
+
+#include "MCAL/PORT/PORT.h"
+#include "MCAL/PORT/PORT_PBcfg.h"
+#include "MCAL/Interrupts/interrupts.h"
+#include "MCAL/ADC/ADC.h"
+#include "MCAL/USART/UART.h"
+#include "MCAL/SPI/SPI.h"
+#include "MCAL/DIO/DIO.h"
+#include "MCAL/DIO/DIO_PBcfg.h"
+#include "MCAL/ADC/ADC.h"
+#include "MCAL/GPT/GPT.h"
+#include "MCAL/ICU/ICU.h"
+#include "HAL/KEYPAD/KeyPad.h"
+#include "HAL/LCD/LCD.h"
+#include "HAL/MOTORS/DCMotor.h"
+#include "HAL/MOTORS/StepperMotor.h"
+#include "HAL/7SEGMENT/sevenSegment.h"
+#include "MCAL/I2C/i2c.h"
+#include "MCAL/ICU/ICU.h"
+#include "MCAL/PWM/PWM.h"
+#include "MCAL/ADC/ADC.h"
+
+
+#endif /* MAIN_H_ */
