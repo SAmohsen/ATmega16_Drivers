af978f3030c9b39f966d28d81e6b78dc255bea01
Sayed Mohsen <48262280+SAmohsen@users.noreply.github.com>
2020-01-04 11:30:21 +0200

    Update ADC Driver

--- MCAL/ADC/ADC.c
+++ MCAL/ADC/ADC.c
@@ -1,146 +1,99 @@
-/*
- * ADC.c
+ /******************************************************************************
  *
- *  Created on: Nov 10, 2019
- *      Author: sayed
- */
+ * Module: ADC
+ *
+ * File Name: adc.c
+ *
+ * Description: Source file for  ADC driver
+ *
+ * Author: sayed mohsen
+ *
+ *******************************************************************************/
 
 
 #include "ADC.h"
 
-static uint8 Adc_Status = ADC_NOT_INITIALIZED ;
-static Adc_ConfigChannel *Adc_ConfiguredChannels = NULL_PTR ;
+static uint8 Adc_Status = ADC_NOT_INITIALIZED ;/*static global variable for ADC status */
+static Adc_ConfigType *Adc_configureChannels = NULL_PTR;/*static global variable for ADC Configurations*/
+static volatile uint16 g_adcResult = 0 ;/*global variable to hold ADC Result*/
 
-void Adc_Init(const Adc_ConfigChannel * Config_Ptr){
-	boolean error = FALSE ;
-	if (NULL_PTR == Config_Ptr)
-	{
-		error = TRUE ;
 
-	}
 
-	else
-	{
-		/*NO Action Required */
-	}
+/*******************************************************************************
+ *                          ISR's Definitions                                  *
+ *******************************************************************************/
+ISR(ADC_vect) {
+	g_adcResult = ADC;/*Read ADC after conversion complete */
+}/*flag cleared automatically after executing ISR*/
 
-	if (FALSE == error)
-	{
-		Adc_ConfiguredChannels = Config_Ptr ;
 
-		/* ADC Enable */
-		SET_BIT(ADCSRA,ADEN);
 
-		switch (Adc_ConfiguredChannels->refrence_volt)
+/*******************************************************************************
+ *                          Functions Definitions                              *
+ *******************************************************************************/
+
+void Adc_init(const Adc_ConfigType *Config_Ptr) {
+	if (NULL_PTR != Config_Ptr) {
+		Adc_configureChannels = Config_Ptr;
+		SET_BIT(ADCSRA, ADEN);/*Enable ADC*/
+		ADMUX  = (ADMUX & 0x3F)  | ((Adc_configureChannels->refrence_volt & 0x03) << 6);/*set ADC reference volt */
+		ADCSRA = (ADCSRA & 0xF8) | (Adc_configureChannels->prescale & 0x07);/*set ADC prescale value ADC must operate in range 50-200Khz*/
+		switch (Adc_configureChannels->aligment)/*ADC result Alignment*/
 		{
-		case Aref: CLR_BIT(ADMUX,REFS0) ;
-				   CLR_BIT(ADMUX,REFS1) ;
-
-		break ;
-
-		case AVCC: CLR_BIT(ADMUX,REFS0) ;
-				   SET_BIT(ADMUX,REFS1) ;
-		break ;
-
-		case internal:SET_BIT(ADMUX,REFS0) ;
-		   	   	   	  SET_BIT(ADMUX,REFS1) ;
-		break ;
-		}
-
-		switch (Adc_ConfiguredChannels->aligment)
-		{
-		case ADC_ALIGN_LEFT:SET_BIT(ADMUX,ADLAR) ;
-
-		break ;
-		case ADC_ALIGN_RIGHT:CLR_BIT(ADMUX,ADLAR) ;
-		break ;
-		}
-		/*bit masking Always A better option :"D */
-
-		switch (Adc_ConfiguredChannels->prescale)
-		{
-		case Prescale_2 :SET_BIT(ADCSRA,ADPS0);
-						 CLR_BIT(ADCSRA,ADPS1);
-						 CLR_BIT(ADCSRA,ADPS2);
-		break ;
-
-		case Prescale_4:CLR_BIT(ADCSRA,ADPS0);
-						SET_BIT(ADCSRA,ADPS1);
-						CLR_BIT(ADCSRA,ADPS2);
-		break ;
-
-		case Prescale_8: SET_BIT(ADCSRA,ADPS0);
-						 SET_BIT(ADCSRA,ADPS1);
-						 CLR_BIT(ADCSRA,ADPS2);
+		case ADC_ALIGN_LEFT:SET_BIT(ADMUX,ADLAR);
 		break;
-
-		case Prescale_16:CLR_BIT(ADCSRA,ADPS0);
-						CLR_BIT(ADCSRA,ADPS0);
-						SET_BIT(ADCSRA,ADPS2);
-
-		break ;
-
-
-		case Prescale_32:SET_BIT(ADCSRA,ADPS0);
-						 CLR_BIT(ADCSRA,ADPS1);
-						 SET_BIT(ADCSRA,ADPS2);
-		break ;
-
-		case Prescale_64:CLR_BIT(ADCSRA,ADPS0);
-						 SET_BIT(ADCSRA,ADPS1);
-						 SET_BIT(ADCSRA,ADPS2);
-		break ;
-
-		case Prescale_128: SET_BIT(ADCSRA,ADPS0);
-						   SET_BIT(ADCSRA,ADPS1);
-						   SET_BIT(ADCSRA,ADPS2);
-		break ;
+		case ADC_ALIGN_RIGHT:CLR_BIT(ADMUX,ADLAR);
+		break;
 		}
-		Adc_Status = ADC_INITIALIZED ;
-	}
-	else
-	{
-		/*NO Action Required */
-	}
+		if (Adc_interrupts == Adc_configureChannels->mode) {
+			SET_BIT(ADCSRA, ADIE);/*enable ADC interrupts*/
+		}
+		if ( ADC_AUTO_TRIGGERING_MODE == STD_ON) {
+			SET_BIT(ADCSRA, ADATE);/*Enable ADC auto triggering mode */
+			SFIOR = (Adc_configureChannels->triggeringmode << 5);/*set ADC Triggering Mode*/
+		}
 
-}
-
-
-void Adc_DeInit(){
-	Adc_ConfiguredChannels = NULL_PTR ;
-}
-
-Adc_ValueGroupType Adc_StartConversion(Adc_Channel channel){
-	boolean error = FALSE ;
-	if (ADC_NOT_INITIALIZED ==Adc_Status)
-	{
-		/* report ADC  not initialized Error*/
-		error = TRUE ;
-	}
-	else
-	{
-
-	}
-	if (NUMBER_ADC_CHANNELS <= channel)
-	{
-		/*Error parameters*/
-		error  = TRUE ;
-	}
-	else
-	{
-
-	}
-
-	if (FALSE == error)
-	{
-		SET_BIT(ADCSRA,ADSC);
-		ADMUX = (ADMUX & 0xE0 ) | (channel & 0x07);
-		while(BIT_IS_CLEAR(ADCSRA,ADIF));//wait until Adc Finish Conversion
-		SET_BIT(ADCSRA,ADIF);//Clear Flag by writing 1 in it
-		return  ADC ;
-	}
-	else
-	{
-
+		Adc_Status = ADC_INITIALIZED;
 	}
 }
+
+
+Adc_ValueGroupType Adc_readChannel(Adc_Channel channel) {
+	Adc_ValueGroupType Adc_result = 0;
+	if (ADC_INITIALIZED == Adc_Status) {
+		ADMUX = (ADMUX & 0xE0) | (channel & 0x07);/*set correct channel*/
+		if (STD_OFF == ADC_AUTO_TRIGGERING_MODE)
+		{
+			if (Adc_interrupts == Adc_configureChannels->mode)
+			{
+				SET_BIT(ADCSRA, ADSC);/*start conversion*/
+			}
+			else if (Adc_polling == Adc_configureChannels->mode)
+			{
+				SET_BIT(ADCSRA, ADSC);/*start conversion*/
+				while (BIT_IS_CLEAR(ADCSRA, ADIF)); /*wait until ADC Finish Conversion*/
+				SET_BIT(ADCSRA, ADIF); /* Clear Flag by writing 1 in it*/
+				Adc_result = ADC;/*read ADC Value*/
+			}
+			else
+			{
+				/*no Action Required*/
+			}
+
+		}
+	}
+
+	return Adc_result;
+}
+
+void Adc_enableInterrupt(void) {
+	SET_BIT(ADCSRA, ADIE);/*enable ADC interrupts*/
+}
+void Adc_disableInterrupt(void) {
+	CLR_BIT(ADCSRA, ADIE);/*disable ADC interrupts*/
+}
+
+Adc_ValueGroupType Adc_getAdcResult(void){
+	return g_adcResult;
+}
+
