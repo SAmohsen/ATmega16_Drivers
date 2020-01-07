83997430c75aa5b16ccf44ac9c8d45ddba38b501
Sayed Mohsen <48262280+SAmohsen@users.noreply.github.com>
2020-01-07 20:00:58 +0200

    update ICU driver

--- MCAL/ICU/ICU.c
+++ MCAL/ICU/ICU.c
@@ -7,91 +7,73 @@
 
 #include "ICU.h"
 
-static volatile  void (*g_callbackptr) (void) =NULL_PTR ;
-ISR(TIMER1_CAPT_vect)
-{
+static volatile  void (*Icu_Handler) (void) =NULL_PTR ;/* Global variables to hold the address of the call back function in the application */
 
-	//PORTA = 0xFF;
-	if (NULL_PTR != g_callbackptr)
+	ISR(TIMER1_CAPT_vect)
+{
+	if (NULL_PTR != Icu_Handler)
 	{
-		//PORTB = 0xFF;
-		(*g_callbackptr)();
+		/* Call the Call Back function in the application after the edge is detected */
+		(*Icu_Handler)();
 	}
 }
 
 void Icu_Init(Icu_ConfigChannel *Icu_ConfigPtr) {
-	boolean error = FALSE;
-	if (NULL_PTR==Icu_ConfigPtr)
-	{
-		error = TRUE;
-	}
-	else
-	{
-
-	}
-	if (FALSE ==error)
+	if (NULL_PTR != Icu_ConfigPtr)
 
 	{
-	/***********non PWM mode**********/
-		TCCR1A = (1<<FOC1A) | (1<<FOC1B);
+		TCCR1A = (1 << FOC1A) | (1 << FOC1B);/*non PWM mode*/
 
-	/*********Icu edge *************/
-	if (Icu_RISING_EDGE ==Icu_ConfigPtr->edge )
-	{
-		SET_BIT(TCCR1B, ICES1);
+		if (Icu_RISING_EDGE == Icu_ConfigPtr->edge) {
+			SET_BIT(TCCR1B, ICES1);/*Rising edge notification*/
+		} else if (ICu_FALLING_EDGE == Icu_ConfigPtr->edge) {
+			CLR_BIT(TCCR1B, ICES1);/*Falling Edge  notification*/
+		} else {
+
+		}
+
+		switch (Icu_ConfigPtr->prescale)/*ICU PRESCALE Value*/
+		{
+		case Icu_no_prescale:
+			SET_BIT(TCCR1B, CS10);
+			CLR_BIT(TCCR1B, CS11);
+			CLR_BIT(TCCR1B, CS12);
+
+			break;
+		case Icu_Prescale_8:
+			CLR_BIT(TCCR1B, CS10);
+			SET_BIT(TCCR1B, CS11);
+			CLR_BIT(TCCR1B, CS12);
+			break;
+
+		case Icu_Prescale_64:
+			SET_BIT(TCCR1B, CS10);
+			SET_BIT(TCCR1B, CS11);
+			CLR_BIT(TCCR1B, CS12);
+			break;
+
+		case Icu_Prescale_256:
+			CLR_BIT(TCCR1B, CS10);
+			CLR_BIT(TCCR1B, CS11);
+			SET_BIT(TCCR1B, CS12);
+			break;
+		case Icu_Prescale_1024:
+			SET_BIT(TCCR1B, CS10);
+			CLR_BIT(TCCR1B, CS11);
+			SET_BIT(TCCR1B, CS12);
+			break;
+		}
+		SET_BIT(TIMSK, TICIE1);/*enable icu interrupts*/
+		TCNT1 = 0;/* Initial Value for Timer1 */
+		ICR1 = 0;/* Initial Value for the input capture register */
 	}
-	else if (ICu_FALLING_EDGE == Icu_ConfigPtr->edge)
-	{
-		CLR_BIT(TCCR1B, ICES1);
-	}
-	else
-	{
-
-	}
-	/*******Icu prescale************/
-
-	switch(Icu_ConfigPtr->prescale)
-	{
-	case Icu_no_prescale: SET_BIT(TCCR1B,CS10);
-					      CLR_BIT(TCCR1B,CS11);
-					      CLR_BIT(TCCR1B,CS12);
-
-	break;
-	case Icu_Prescale_8:  CLR_BIT(TCCR1B,CS10);
-					  	  SET_BIT(TCCR1B,CS11);
-					  	  CLR_BIT(TCCR1B,CS12);
-	break;
-
-	case Icu_Prescale_64: SET_BIT(TCCR1B,CS10);
-					      SET_BIT(TCCR1B,CS11);
-					      CLR_BIT(TCCR1B,CS12);
-	break;
-
-	case Icu_Prescale_256: CLR_BIT(TCCR1B,CS10);
-					       CLR_BIT(TCCR1B,CS11);
-					       SET_BIT(TCCR1B,CS12);
-	break;
-	case Icu_Prescale_1024: SET_BIT(TCCR1B,CS10);
-						    CLR_BIT(TCCR1B,CS11);
-						    SET_BIT(TCCR1B,CS12);
-	break;
-	}
-	}
-	else
-	{
-
-	}
-	/* Initial Value for Timer1 */
-	TCNT1 = 0;
-
-	/* Initial Value for the input capture register */
-	ICR1 = 0;
-	//TIMSK |= (1<<TICIE1);
-
-
 }
 
+/*set edge notification*/
 void Icu_SetEdgeDetection(Icu_EdgeNotificationType edge) {
+	/*
+	 * insert the required edge type in ICES1 bit in TCCR1B Register
+	*/
 	if (Icu_RISING_EDGE == edge)
 	{
 		SET_BIT(TCCR1B, ICES1);
@@ -102,36 +84,49 @@ void Icu_SetEdgeDetection(Icu_EdgeNotificationType edge) {
 	}
 	else
 	{
-
+		/*no Action Required*/
 	}
 
 
 }
+/*
+ * Description: Function to set the Call Back function address.
+ */
 void Icu_SetCallback(void (*a_ptr)(void)) {
 	if (a_ptr != NULL_PTR)
 	{
-		g_callbackptr = a_ptr;
+
+		Icu_Handler = a_ptr;
 	}
 }
 
 void Icu_EnableInterrupts(void) {
 		SET_BIT(TIMSK, TICIE1);
 }
+/*
+ * Description: Function to get the Timer1 Value when the input is captured
+ *              The value stored at Input Capture Register ICR1
+ */
 uint16 Icu_getInputCaptureValue(void)
 {
 	return ICR1;
 }
+/*
+ * Description: Function to clear the Timer1 Value to start count from ZERO
+ */
 void Icu_clearTimeValue(void)
 {
 	ICR1 = 0 ;
 }
+/*
+ * Description: Function to disable the Timer1 to stop the ICU Driver
+ */
 
 void Icu_Deinit(void) {
 
 	TCNT1 = 0;
 	TCCR1A =0 ;
 	TCCR1B=0;
-	CLR_BIT(TIMSK, TICIE1);
-	g_callbackptr = NULL_PTR;
-
+	CLR_BIT(TIMSK, TICIE1);/*clear Icu interrupts*/
+	Icu_Handler = NULL_PTR;
 }
